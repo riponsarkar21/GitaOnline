ac5596745b435b8a38672d684416780102e25cb4
riponsarkar21 <141989340+riponsarkar21@users.noreply.github.com>
2026-02-23 18:33:14 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+// Service Worker for Gita Online PWA
+const CACHE_NAME = 'gita-online-v1';
+const urlsToCache = [
+  './',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          return caches.match('./');
+        });
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
